14b8f3dac9a47a3876b133712de1b47580089e06
lestercarpay <lester.carpay@gmail.com>
2024-02-24 16:09:41 +0100

    Added main method.

--- keurenspel.py
+++ keurenspel.py
@@ -10,49 +10,51 @@ def get_radius(i, frame_rate=frame_rate):
     return int(1000/(i//frame_rate + 20))
 
 
-# activate the pygame library
-# initiate pygame and give permission
-# to use pygame's functionality.
-pygame.init()
+def play_game():
+    pygame.init()
+    image = TemplateImage.get_random_image()
+    win = pygame.display.set_mode(image.get_size())
 
-# create the display surface object
-# (x, y) is the height and width of pygame window
+    # set the pygame window name
+    pygame.display.set_caption("Kleurenspel")
 
-image = TemplateImage.get_random_image()
-win = pygame.display.set_mode(image.get_size())
+    done = False
+    clock = pygame.time.Clock()
+    circle_positions = []
+    circle_colors = []
+    mouse_pressed = False
+    mouse_position = (0, 0)
 
-# set the pygame window name
-pygame.display.set_caption("Kleurenspel")
+    while not done:
+        win.fill((250, 250, 250))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pressed = True
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse_pressed = False
+            elif event.type == pygame.MOUSEMOTION:
+                mouse_position = event.pos
 
-done = False
-clock = pygame.time.Clock()
-circle_positions = []
-circle_colors = []
-mouse_pressed = False
-mouse_position = (0, 0)
+        if mouse_pressed:
+            circle_positions.append(mouse_position)
+            col = image.get_circle_color(mouse_position, get_radius(len(circle_positions) - 1))
+            circle_colors.append(col)
 
-while not done:
-    win.fill((250, 250, 250))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pressed = True
-        elif event.type == pygame.MOUSEBUTTONUP:
-            mouse_pressed = False
-        elif event.type == pygame.MOUSEMOTION:
-            mouse_position = event.pos
+        for i in range(len(circle_positions)):
+            position = circle_positions[i]
+            color = circle_colors[i]
+            pygame.draw.circle(win, color, position, get_radius(i))
+        pygame.display.flip()
+        clock.tick(frame_rate)
 
-    if mouse_pressed:
-        circle_positions.append(mouse_position)
-        col = image.get_circle_color(mouse_position, get_radius(len(circle_positions) - 1))
-        circle_colors.append(col)
+    pygame.quit()
 
-    for i in range(len(circle_positions)):
-        position = circle_positions[i]
-        color = circle_colors[i]
-        pygame.draw.circle(win, color, position, get_radius(i))
-    pygame.display.flip()
-    clock.tick(frame_rate)
 
-pygame.quit()
+def main():
+    play_game()
+
+
+if __name__== "__main__":
+    main()
